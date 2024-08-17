5f83125ad6c1ac70abf6b07d1f7e8a6ee076f8fb
Pue90 <43031720+PuenaruIonutMarian@users.noreply.github.com>
2024-08-17 11:39:05 +0200

    style fixes pagination crawling problem fixed

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -4,7 +4,7 @@ type PaginationProps = {
   onPageChange: (page: number) => void;
 };
 
- //Provides navigation controls for paginating through the data.
+
 const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage, onPageChange }) => {
   const generatePageNumbers = () => {
     const pages = [];
@@ -22,7 +22,7 @@ const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage, onPage
         for (let i = 1; i <= maxPagesToShow; i++) {
           pages.push(i);
         }
-        pages.push( '...', totalPages);
+        pages.push('...', totalPages);
       } else if (currentPage >= totalPages - maxPagesToShow + 2) {
         // Near the end
         pages.push(1, '...');
@@ -46,40 +46,40 @@ const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage, onPage
 
   return (
     <div className='pagination'>
-<button 
-  className="button-backward" 
-  disabled={currentPage === 1} 
-  onClick={() => onPageChange(currentPage - 1)}
-  aria-label="Previous Page"
->
-  &laquo;
-</button>
+      <button 
+        className="button-backward" 
+        disabled={currentPage === 1} 
+        onClick={() => onPageChange(currentPage - 1)}
+        aria-label="Previous Page"
+      >
+        &laquo;
+      </button>
 
-<div className="pages">
-  {generatePageNumbers().map((page, index) => (
-    <a
-      key={index}
-      className={page === currentPage ? 'active' : ''}
-      onClick={() => handlePageClick(page)}
-      style={{ cursor: page === '...' ? 'default' : 'pointer', margin: '0 5px' }}
-      aria-current={page === currentPage ? 'page' : undefined}
-    >
-      {page}
-    </a>
-  ))}
-</div>
+      <div className="pages">
+        {generatePageNumbers().map((page, index) => (
+          <button
+            key={index}
+            className={page === currentPage ? 'active' : ''}
+            onClick={() => handlePageClick(page)}
+            style={{ cursor: page === '...' ? 'default' : 'pointer', margin: '0 5px' }}
+            aria-current={page === currentPage ? 'page' : undefined}
+            aria-label={page === '...' ? undefined : `Page ${page}`}
+          >
+            {page}
+          </button>
+        ))}
+      </div>
 
-<button 
-  className="button-forward" 
-  disabled={currentPage === totalPages} 
-  onClick={() => onPageChange(currentPage + 1)}
-  aria-label="Next Page"
->
-  &raquo;
-</button>
+      <button 
+        className="button-forward" 
+        disabled={currentPage === totalPages} 
+        onClick={() => onPageChange(currentPage + 1)}
+        aria-label="Next Page"
+      >
+        &raquo;
+      </button>
     </div>
   );
 };
 
 export default Pagination;
-
